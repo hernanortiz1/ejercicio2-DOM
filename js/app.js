b0859e9862e6d52952eb6b49fb3f02ff39618ec0
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-05-18 00:34:03 -0300

    agregue metodo obtenerGeneracion y modifique funcion mostrarGeneracion

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-class persona {
+class Persona {
   #nombre;
   #edad;
   #dni;
@@ -16,43 +16,66 @@ class persona {
     this.#altura = altura;
     this.#anioNacimiento = anioNacimiento;
   }
+
+  get getNombre() {
+    return this.#nombre;
+  }
+
+  get getEdad() {
+    return this.#edad;
+  }
+
+  get getAnioNacimiento() {
+    return this.#anioNacimiento;
+  }
+
+  obtenerGeneracion() {
+    const anio = this.#anioNacimiento;
+    if (anio >= 1930 && anio <= 1948)
+      return ["Silent Generation", "Austeridad"];
+    if (anio >= 1949 && anio <= 1968) return ["Baby Boom", "Ambición"];
+    if (anio >= 1969 && anio <= 1980)
+      return ["Generación X", "Obsesión por el éxito"];
+    if (anio >= 1981 && anio <= 1993) return ["Generación Y", "Frustración"];
+    if (anio >= 1994 && anio <= 2010) return ["Generación Z", "Irreverencia"];
+  }
 }
 
 const mostrarGeneracion = (e) => {
   e.preventDefault();
-  // const anioNacimiento = document.getElementById("fechaNacimiento").value.trim();
-  const datos = document.getElementsByTagName("input");
-  console.log(datos);
 
-  if (anioNacimiento >= 1930 && anioNacimiento <= 1948) {
-    document.writeln(
-      `<p>${
-        this.#nombre
-      } pertenece a Silent Generation, Caracteristica: Austeridad</p>`
-    );
-  } else if (anioNacimiento >= 1949 && anioNacimiento <= 1968) {
-    document.writeln(
-      `<p>${this.#nombre} pertenece a Baby Boom, Caracteristica: Ambición</p>`
-    );
-  } else if (anioNacimiento >= 1969 && anioNacimiento <= 1980) {
-    document.writeln(
-      `<p>${
-        this.#nombre
-      } pertenece a Generación X, Caracteristica: Obsesión por el exito</p>`
-    );
-  } else if (anioNacimiento >= 1981 && anioNacimiento <= 1993) {
-    document.writeln(
-      `<p>${
-        this.#nombre
-      } pertenece a Generación Y, Caracteristica: Frustración</p>`
-    );
-  } else if (anioNacimiento >= 1994 && anioNacimiento <= 2010) {
-    document.writeln(
-      `<p>${
-        this.#nombre
-      } pertenece a Generación Z, Caracteristica: Irreverencia</p>`
-    );
-  }
+  const btnMostrarGeneracion = document.getElementById("contenedorGeneracion");
+  btnMostrarGeneracion.classList.toggle("d-none");
+
+  const datos = document.getElementsByTagName("input");
+  const nombre = datos[0].value;
+  const edad = parseInt(datos[1].value);
+  const dni = datos[2].value;
+  const peso = datos[4].value;
+  const altura = datos[5].value;
+  const anioNacimiento = parseInt(datos[6].value);
+
+  const persona = new Persona(
+    nombre,
+    edad,
+    dni,
+    sexo,
+    peso,
+    altura,
+    anioNacimiento
+  );
+  const [generacion, caracteristica] = persona.obtenerGeneracion();
+
+  const parrafoMostrarGeneracion = document.getElementById(
+    "resultadoGeneracion"
+  );
+  parrafoMostrarGeneracion.innerHTML = `<ul>
+  <li>Nombre: ${persona.getNombre}</li>
+  <li>Edad: ${persona.getEdad}</li>
+  <li>Año de nacimiento: ${persona.getAnioNacimiento}</li>
+  <li>Generación: ${generacion}</li>
+  <li>Caracteristica: ${caracteristica}</li>
+  </ul>`;
 };
 
 const btnMostrarGeneracion = document.getElementById("btnMostrarGeneracion");
